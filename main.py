84d04531dfadcc17a80bf3bbb94be6b2703cef77
koziatom <ttomek.koziak@gmail.com>
2020-03-26 11:43:00 +0100

    GUI added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from scapy.layers.inet import ICMP, IP, TCP, sr1
 import random
 
 
-def cmd_ping(ip_dst, ip_src, seq, id, verbose, timeout):
+def cmd_ping(ip_dst, ip_src, seq, icmp_id):
     conf.verb = False
 
     layer3 = IP()
@@ -22,32 +22,27 @@ def cmd_ping(ip_dst, ip_src, seq, id, verbose, timeout):
     layer4 = ICMP()
     layer4.type = 8  # echo-request
     layer4.code = 0
-    layer4.id = id
+    layer4.id = icmp_id
     layer4.seq = seq
-
     pkt = layer3 / layer4 / b"abcdefghijklmn opqrstuvwabcdefg hi"
-    ans = sr1(pkt, timeout=timeout)
-    if ans:
-        if verbose:
-            ans.show()
-        else:
-            print(ans.summary())
-            del ans
-    else:
-        print('Timeout')
+    send(pkt)
+    print("Ping Sent")
 
 
-def cmd_tcpip(ip):
+def cmd_tcpip(ip_src, ip_dst, reserved):
     layer3 = IP()
-    layer3.src = "192.168.1.107"
-    layer3.dst = ip
+    layer3.src = ip_src
+    layer3.dst = ip_dst
     layer3.ttl = 255
     layer3.ihl = 5
 
     layer4 = TCP()
     layer4.dport = 80
     layer4.sport = 20
-    layer4.reserved = 0b0111
+    num = int(reserved, 2)
+    binary_num = bin(num)
+    print(binary_num)
+    layer4.reserved = num
     #    layer4.flags = "S"
     layer4.dataofs = 5
     layer4.flags = 'S'
@@ -60,27 +55,73 @@ def cmd_tcpip(ip):
 # window object
 app = Tk()
 
-# texts listners
+# ICMP texts listners
 src_text = StringVar()
-src_label = Label(app, text='Source IP: ', font=('bold', 12), pady=20)
+src_label = Label(app, text='Source IP: ', font=('bold', 12), pady=10)
 src_label.grid(row=0, column=0, sticky=W)
 src_entry = Entry(app, textvariable=src_text)
 src_entry.grid(row=0, column=1)
 
 dst_text = StringVar()
-dst_label = Label(app, text='Destination IP: ', font=('bold', 12), pady=20)
+dst_label = Label(app, text='Destination IP: ', font=('bold', 12), pady=10)
 dst_label.grid(row=1, column=0, sticky=W)
 dst_entry = Entry(app, textvariable=dst_text)
 dst_entry.grid(row=1, column=1)
 
+id_text = IntVar()
+id_label = Label(app, text='Ping ID: ', font=('bold', 12), pady=10)
+id_label.grid(row=0, column=2, sticky=W)
+id_entry = Entry(app, textvariable=id_text)
+id_entry.grid(row=0, column=3)
+
+seq_text = IntVar()
+seq_label = Label(app, text='Ping Seq: ', font=('bold', 12), pady=10)
+seq_label.grid(row=1, column=2, sticky=W)
+seq_entry = Entry(app, textvariable=seq_text)
+seq_entry.grid(row=1, column=3)
+
+# TCP texts listners
+src_TCP_text = StringVar()
+src_label = Label(app, text='Source IP: ', font=('bold', 12), pady=10)
+src_label.grid(row=4, column=0, sticky=W)
+src_entry = Entry(app, textvariable=src_TCP_text)
+src_entry.grid(row=4, column=1)
+
+dst_TCP_text = StringVar()
+dst_label = Label(app, text='Destination IP: ', font=('bold', 12), pady=10)
+dst_label.grid(row=5, column=0, sticky=W)
+dst_entry = Entry(app, textvariable=dst_TCP_text)
+dst_entry.grid(row=5, column=1)
+
+reserved_bits_text = StringVar()
+reserved_bits_label = Label(app, text='Reserved Bits: ', font=('bold', 12), pady=10)
+reserved_bits_label.grid(row=4, column=2, sticky=W)
+reserved_bits_entry = Entry(app, textvariable=reserved_bits_text)
+reserved_bits_entry.grid(row=4, column=3)
+
 
 def cmd():
-    print("click")
+    print(dst_text.get())
+    print(src_text.get())
+    print(seq_text.get())
+    print(id_text.get())
+    cmd_ping(dst_text.get(), src_text.get(), seq_text.get(), id_text.get())
+
+
+def cmd_TCP():
+    print(dst_TCP_text.get())
+    print(src_TCP_text.get())
+    print(reserved_bits_text.get())
+    cmd_tcpip(src_TCP_text.get(), dst_TCP_text.get(), reserved_bits_text.get())
 
 
 # buttons
-icmp_steg_btn = Button(app, text='Send Ping', width=12, command=cmd)
+icmp_steg_btn = Button(app, text='Send Ping', width=12, command=cmd, padx=10)
 icmp_steg_btn.grid(row=3, column=0)
+
+# buttons
+icmp_steg_btn = Button(app, text='Send Ping', width=12, command=cmd_TCP, padx=10)
+icmp_steg_btn.grid(row=6, column=0)
 
 app.title('Stego Tester')
 app.geometry('700x250')
