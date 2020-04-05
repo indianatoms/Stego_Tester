9575ca4b7b9debececf02473952d7b85a25a16c2
koziatom <ttomek.koziak@gmail.com>
2020-04-05 12:49:08 +0200

    ttl added

--- main.py
+++ main.py
@@ -29,11 +29,14 @@ def cmd_ping(ip_dst, ip_src, seq, icmp_id):
     print("Ping Sent")
 
 
-def cmd_tcpip(ip_src, ip_dst, reserved, seq_num, window, urg_ptr, flags, payload, src_port):
+def cmd_tcpip(ip_src, ip_dst, TOS, ttl, reserved, seq_num, window, urg_ptr, flags, payload, src_port):
     layer3 = IP()
     layer3.src = ip_src
     layer3.dst = ip_dst
-    layer3.ttl = 255
+    tos_num = int(TOS, 2)
+    print(tos_num)
+    layer3.tos = tos_num
+    layer3.ttl = ttl
     layer3.ihl = 5
 
     layer4 = TCP()
@@ -87,6 +90,7 @@ seq_entry = Entry(app, textvariable=seq_text)
 seq_entry.grid(row=1, column=3)
 
 # TCP texts listners
+#IP
 src_TCP_text = StringVar()
 src_label = Label(app, text='Source IP: ', font=('bold', 12), pady=10)
 src_label.grid(row=4, column=0, sticky=W)
@@ -99,47 +103,60 @@ dst_label.grid(row=5, column=0, sticky=W)
 dst_entry = Entry(app, textvariable=dst_TCP_text)
 dst_entry.grid(row=5, column=1)
 
+TOS_text = StringVar()
+TOS_label = Label(app, text='TOS: ', font=('bold', 12), pady=10)
+TOS_label.grid(row=4, column=2, sticky=W)
+TOS_entry = Entry(app, textvariable=TOS_text)
+TOS_entry.grid(row=4, column=3)
+
+ttl_text = StringVar()
+ttl_label = Label(app, text='ttl: ', font=('bold', 12), pady=10)
+ttl_label.grid(row=5, column=2, sticky=W)
+ttl_entry = Entry(app, textvariable=ttl_text)
+ttl_entry.grid(row=5, column=3)
+
+#TCP
 reserved_bits_text = StringVar()
 reserved_bits_label = Label(app, text='Reserved Bits: ', font=('bold', 12), pady=10)
-reserved_bits_label.grid(row=4, column=2, sticky=W)
+reserved_bits_label.grid(row=6, column=0, sticky=W)
 reserved_bits_entry = Entry(app, textvariable=reserved_bits_text)
-reserved_bits_entry.grid(row=4, column=3)
+reserved_bits_entry.grid(row=6, column=1)
 
 window_text = StringVar()
 window_label = Label(app, text='Window: ', font=('bold', 12), pady=10)
-window_label.grid(row=5, column=2, sticky=W)
+window_label.grid(row=7, column=0, sticky=W)
 window_entry = Entry(app, textvariable=window_text)
-window_entry.grid(row=5, column=3)
+window_entry.grid(row=7, column=1)
 
 urgent_pointer_text = StringVar()
 urgent_pointer_label = Label(app, text='Urgent Pointer: ', font=('bold', 12), pady=10)
-urgent_pointer_label.grid(row=4, column=4, sticky=W)
+urgent_pointer_label.grid(row=6, column=2, sticky=W)
 urgent_pointer_entry = Entry(app, textvariable=urgent_pointer_text)
-urgent_pointer_entry.grid(row=4, column=5)
+urgent_pointer_entry.grid(row=6, column=3)
 
 seq_TCP_text = StringVar()
 seq_TCP_label = Label(app, text='Seq Number: ', font=('bold', 12), pady=10)
-seq_TCP_label.grid(row=5, column=4, sticky=W)
+seq_TCP_label.grid(row=7, column=2, sticky=W)
 seq_TCP_entry = Entry(app, textvariable=seq_TCP_text)
-seq_TCP_entry.grid(row=5, column=5)
+seq_TCP_entry.grid(row=7, column=3)
 
 flags_text = StringVar()
 flags_label = Label(app, text='Flags: ', font=('bold', 12), pady=10)
-flags_label.grid(row=4, column=6, sticky=W)
+flags_label.grid(row=6, column=4, sticky=W)
 flags_entry = Entry(app, textvariable=flags_text)
-flags_entry.grid(row=4, column=7)
+flags_entry.grid(row=6, column=5)
 
 payload_text = StringVar()
 payload_label = Label(app, text='Payload: ', font=('bold', 12), pady=10)
-payload_label.grid(row=5, column=6, sticky=W)
+payload_label.grid(row=7, column=4, sticky=W)
 payload_entry = Entry(app, textvariable=payload_text)
-payload_entry.grid(row=5, column=7)
+payload_entry.grid(row=7, column=5)
 
 sport_text = IntVar()
 sport_label = Label(app, text='Source Port: ', font=('bold', 12), pady=10)
-sport_label.grid(row=4, column=8, sticky=W)
+sport_label.grid(row=6, column=6, sticky=W)
 sport_entry = Entry(app, textvariable=sport_text)
-sport_entry.grid(row=4, column=9)
+sport_entry.grid(row=6, column=7)
 
 
 def cmd():
@@ -157,7 +174,7 @@ def cmd_TCP():
     print(seq_TCP_text.get())
     print(window_text.get())
     print(urgent_pointer_text.get())
-    cmd_tcpip(src_TCP_text.get(), dst_TCP_text.get(), reserved_bits_text.get(), seq_TCP_text.get(), window_text.get(), urgent_pointer_text.get(), flags_text.get(),
+    cmd_tcpip(src_TCP_text.get(), dst_TCP_text.get(), TOS_text.get(), ttl_text.get(), reserved_bits_text.get(), seq_TCP_text.get(), window_text.get(), urgent_pointer_text.get(), flags_text.get(),
               payload_text.get(), sport_text.get())
 
 
@@ -166,11 +183,11 @@ icmp_steg_btn = Button(app, text='Send Ping', width=12, command=cmd, padx=10)
 icmp_steg_btn.grid(row=3, column=0)
 
 # buttons
-icmp_steg_btn = Button(app, text='Send TCP', width=12, command=cmd_TCP, padx=10)
-icmp_steg_btn.grid(row=6, column=0)
+icmp_steg_btn = Button(app, text='Send TCP/IP', width=12, command=cmd_TCP, padx=10)
+icmp_steg_btn.grid(row=8, column=0)
 
 app.title('Stego Tester')
-app.geometry('1500x250')
+app.geometry('1200x350')
 app.mainloop()
 # cmd_tcpip("192.168.1.104")
 # cmd_ping("192.168.1.104", 4, 128, 1, 0)
