23a1229383535b4e1e2ee9e22694357b725fdf9c
indianatoms <ttomek.koziak@gmail.com>
2021-08-23 16:26:51 +0200

    mqqt

--- mqtt_test.py
+++ mqtt_test.py
@@ -1,6 +1,5 @@
 import paho.mqtt.client as mqtt
 from time import sleep
-<<<<<<< HEAD
 client = mqtt.Client()
 
 #client.on_connect = on_connect
@@ -27,20 +26,3 @@ for usr in v:
     client.loop_stop()
     client.disconnect()
 
-=======
-
-client = mqtt.Client()
-#client.on_connect = on_connect
-#client.on_message = on_message
-#client = mqtt.Client(id)
-#client.username_pw_set(user, password=psw)
-broker = "192.168.1.21"
-payload = "test1"
-topic = "test/topic"
-print("connecting to broker ", broker)
-client.connect(broker, 1883, True)
-client.loop_start()
-client.publish(topic, payload, retain=False)
-client.loop_stop()
-client.disconnect()
->>>>>>> a2809610938646f0f3955cc7e1c059b4908de206
